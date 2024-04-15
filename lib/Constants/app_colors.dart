80b06176a53f53efd8a66af9a1a03db0cd20d31f
Aravind M S <113226859+aravind-m-s@users.noreply.github.com>
2024-04-15 23:35:22 +0530

    Delete lib directory

--- lib/Constants/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColors = Color(0xFF283A90);
-  static Color greyColor = Colors.grey;
-  static Color secondaryGrey = Colors.grey.shade300;
-  static Color teritiaryGrey = Colors.grey.shade200;
-  static const Color backgroundColor = Colors.white;
-}
